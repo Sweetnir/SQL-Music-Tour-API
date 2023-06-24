80465d44bb4d9f0fd5dcc36ad4fe4447024f9075
Damon Herrera <bustinbubs@gmail.com>
2023-06-24 11:25:09 -0700

    adding routes and files

---
+++ models/event.js
@@ -0,0 +1,63 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+const stageevent = require('./stageevent')
+module.exports = (sequelize, DataTypes) => {
+  class Event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(Stage) {
+    // define association here
+    Event.belongsTo(Stage, {
+        foreignKey:'event_id' ,
+        as: 'stages',
+        through: StageEvent
+    })
+    Event.hasMany(MeetGreet, {
+        foreignKey: 'event_id',
+        as: 'meet_greets'
+      })
+      MeetGreet.belongsTo(Event, {
+        foreignKey: 'event_id',
+        as: 'meet_greets'
+      })
+      Event.hasMany(SetTime, {
+        foreignKey: 'event_id',
+        as: 'set_times'
+      })
+    }
+  }
+  Event.init({
+    event_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    start_time: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    end_time: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Event',
+    tableName: 'event',
+    timestamps: false
+  })
+  return Event
+}
